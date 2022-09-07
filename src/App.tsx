e0fb2dfb9be6f2a8129d562f648a2d9b383b1118
Eric Karschner <erickarschner@gmail.com>
2022-09-07 13:45:38 -0400

    Add page title

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,15 @@ import {
   faPhoneFlip,
 } from "@fortawesome/free-solid-svg-icons";
 import { PageResume } from "./pages/PageResume";
+import React from "react";
 
 library.add(fab, faAt, faCalendar, faGlobe, faPhoneFlip, faCalculator);
 
 function App() {
+  React.useEffect(() => {
+    document.title = "Eric Karschner";
+  }, []);
+
   return (
     <BrowserRouter>
       <PageResume />
